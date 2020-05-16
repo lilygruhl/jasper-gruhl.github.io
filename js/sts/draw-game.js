9f4dd2a3d440250505773f10db6faa495690547d
lilygruhl <63130871+lilygruhl@users.noreply.github.com>
2020-05-16 14:47:32 -0700

    Update draw-game.js

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -2,6 +2,6 @@ function drawGame(p) {
     //You can put your game/drawing code here. For a full reference of functions
     //see http://processingjs.org/reference/. You will need to always put processing. before the
     //function. Below is an example ellipse...
-    p.ellipse(200,200,200,2000);
+    p.ellipse(200,200,200,200);
     p.ellipse(200,200,100,100);
 }
