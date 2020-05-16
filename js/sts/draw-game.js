9f97af941fef9237231ff54795f0914340eded9a
lilygruhl <63130871+lilygruhl@users.noreply.github.com>
2020-05-16 12:59:48 -0700

    Update draw-game.js

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -3,5 +3,5 @@ function drawGame(p) {
     //see http://processingjs.org/reference/. You will need to always put processing. before the
     //function. Below is an example ellipse...
     p.ellipse(500,200,200,2000);
-    p.ellipse(800,200,100,100);
+    p.ellipse(800,200,1000,100);
 }
