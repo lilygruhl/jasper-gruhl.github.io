ca3dd745cb2f1fcd6cae6cf2802c245a2cf936c5
Jason Gruhl <jgruhl@gmail.com>
2020-05-16 12:39:19 -0700

    Create draw-game.js

---
+++ js/sts/draw-game.js
@@ -0,0 +1,8 @@
+function drawGame(p) {
+    //You can put your game/drawing code here. For a full reference of functions
+    //see http://processingjs.org/reference/. You will need to always put processing. before the
+    //function. Below is an example ellipse...
+    p.ellipse(560, 460, 550, 350);
+    p.fill(400,20,200);
+    p.text("Hi my name is Lily!",25,73);
+}
