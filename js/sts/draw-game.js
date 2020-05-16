db619b9eb456913f0dd83ab299eff9d6119e801a
lilygruhl <63130871+lilygruhl@users.noreply.github.com>
2020-05-16 14:53:33 -0700

    Update draw-game.js

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -4,4 +4,13 @@ function drawGame(p) {
     //function. Below is an example ellipse...
     p.ellipse(200,200,200,200);
     p.ellipse(200,200,100,100);
+    p.// @pjs preload must be used to preload the image 
+
+    p./* @pjs preload="laDefense.jpg"; */
+
+    p.PImage b;
+
+    p.b = loadImage("laDefense.jpg");
+
+    p.image(b, 0, 0);
 }
