c89cf2876bcacf998918c1b72779c09ab7e8220c
Jason Gruhl <jgruhl@gmail.com>
2020-05-17 15:14:16 -0700

    Made the pickle go down the screen but we thought that the background of the image was white but it was acctuly light gray. UGH!!!

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -1,8 +1,11 @@
+//called once at the start
+function setup(p) {
+    p.background(255,255,255);
+}
+
+//Called 10 times per second
 function drawGame(p) {
-    //You can put your game/drawing code here. For a full reference of functions
-    //see http://processingjs.org/reference/. You will need to always put processing. before the
-    //function. Below is an example ellipse...
-    p.ellipse(560, 460, 550, 350);
-    p.fill(400,20,200);
-    p.image(breadImage, 0, 0);
+    y = y+1
+    p.noStroke();
+    p.image(pickleImage,400,y,70,70);
 }
