a1d64f56ea3e11463468acd1366be49f0908041b
Jason Gruhl <jgruhl@gmail.com>
2020-05-23 16:32:20 -0700

    Converting current state of game to p5js

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -1,17 +1,26 @@
-//called once at the start
-function setup() {
-    y = 0
-    background(255,255,255);
+let hamImage;
+let breadImage;
+let pickleImage;
+let lettuceImage;
+let y = 0;
+
+function preload() {
     hamImage = loadImage("images/ham.jpg");
     breadImage = loadImage("images/dutch-crunch.png");
     pickleImage = loadImage("images/pickle.png");
     lettuceImage = loadImage("images/lettuce.png");
+}
 
+function setup() {
+
+    background(255,255,255);
+    createCanvas(1000, 1000);
 }
 
 //Called 10 times per second
 function draw() {
-    y = y+1
+    y = y+1;
     noStroke();
-    image(pickleImage,400,y,70,70);
+    image(hamImage,0, y, 70, 70);
+
 }
