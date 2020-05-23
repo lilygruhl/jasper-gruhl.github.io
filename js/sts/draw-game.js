33e91acc7cad29f4332f0d84fd1b2a2221a9c753
Jason Gruhl <jgruhl@gmail.com>
2020-05-23 15:55:07 -0700

    Switching to p5js

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -1,11 +1,17 @@
 //called once at the start
-function setup(p) {
-    p.background(255,255,255);
+function setup() {
+    y = 0
+    background(255,255,255);
+    hamImage = loadImage("images/ham.jpg");
+    breadImage = loadImage("images/dutch-crunch.png");
+    pickleImage = loadImage("images/pickle.png");
+    lettuceImage = loadImage("images/lettuce.png");
+
 }
 
 //Called 10 times per second
-function drawGame(p) {
+function draw() {
     y = y+1
-    p.noStroke();
-    p.image(pickleImage,400,y,70,70);
+    noStroke();
+    image(pickleImage,400,y,70,70);
 }
