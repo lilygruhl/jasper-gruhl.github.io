51340ad38ce223e4d8fc8c16b897a65433747d68
jaspergruhl <gruhl.jasper@gmail.com>
2020-06-07 00:12:40 +0000

    made plate move!!!!

--- js/sts/draw-game.js
+++ js/sts/draw-game.js
@@ -5,7 +5,8 @@ let pickleImage;
 let lettuceImage;
 let y = 0;
 let speed = 5;
-
+let plateX = 100
+let plateXSpeed=15
 function preload() {
     hamImage = loadImage("images/ham.jpg");
     breadImage = loadImage("images/dutch-crunch.png");
@@ -25,13 +26,20 @@ function draw() {
     }
     background(255,255,255);
     y = y + speed;
+    drawPlate(plateX, 500, 100, 100)
     image(hamImage,0, y, 70, 70);
     image(lettuceImage, 200, y, 70, 70);
-    drawPlate(100, 500, 100, 100)
 }
 
 function drawPlate(x, y, w, h) {
  image(plateImage, x, y, w, h);
+}
+function keyPressed() {
+  if (keyCode === 74) {
+    plateX = plateX - plateXSpeed;
+  } else if (keyCode === 76) {
+    plateX = plateX + plateXSpeed;
+  }
 }
 
 function mousePressed() {
